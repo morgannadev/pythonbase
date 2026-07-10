80e308184e2bb25f9a6f5b88e8b3954a7a7c6f84
Morganna Giovanelli de Souza <morgcontato@gmail.com>
2026-07-10 13:05:29 -0300

    Adiciona tabuada e interpolação

---
+++ interpolacao.py
@@ -0,0 +1,27 @@
+email_tmpl = """
+Olá, %(nome)s
+
+Tem interesse em comprar %(produto)s?
+
+Este produto é ótimo para resolver %(texto)s.
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponível!
+
+Preço promocional %(preco).2f
+"""
+
+clientes = ["Maria", "João", "Bruno"]
+
+for cliente in clientes:
+    print(
+        email_tmpl % {
+            "nome": cliente,
+            "produto": "caneta",
+            "texto": "Escrever muito bem",
+            "link": "https://canetaslegais.com",
+            "quantidade": 1,
+            "preco": 50.5
+        }
+    )
